3a492c0df94d24e7fdfd46ab78ec086d88057550
Daniel Frederick <frederickd5@udayton.edu>
2026-01-28 12:04:38 -0500

    isntall pygame and added notes

--- PuzzleGame Framework/sol/solution.py
+++ PuzzleGame Framework/sol/solution.py
@@ -3,22 +3,26 @@
 # Below are the functions you need to implement. For the first project, you only need to finish implementing iddfs() 
 # ie iterative deepening depth first search
 
+# run: `python3 -m game.puzzle`
+
+# Successor Funciton - Given current state, it returns all possible next states
 
 # here you need to implement the Iterative Deepening Search Method
 def iterativeDeepening(puzzle):
-    list = []
-    return list
+    # puzzle = 1D list of current state
 
+    # Create movement table
+    # 1. Where "8" appears
+    # 2. All the next possible new possitions for "8"
+    movement = [][]
+    movement[0] = [1,3]
+    movement[4] = [1,3,5,7]
 
-# This will be for next project
+    # The sequence of "8" positions
+    path = [] # Shortest path
+    return path
+
 def astar(puzzle):
     list = []
     return list
-
-
-
-
-
-
-
 
